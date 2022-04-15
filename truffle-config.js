af9b64d70a13e3102b0f90a238074f01d7d0ee63
albus <albinbabuvarghese20@gmail.com>
2022-04-15 13:48:03 +0530

    Changed solc version from 0.5.16 to 0.4.26

--- truffle-config.js
+++ truffle-config.js
@@ -13,6 +13,7 @@ module.exports = {
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+      version: "0.4.26",
       optimizer: {
         enabled: true,
         runs: 200
