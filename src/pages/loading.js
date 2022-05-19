8f656d3bfa4b86cfa151037de8633a7dae54da43
lensmaaan <akhilmohan3868@gmail.com>
2022-05-19 21:02:40 +0530

    loading page added

---
+++ src/pages/loading.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import ReactLoading from "react-loading";
+
+function PreLoader1() {
+  const [data, setData] = useState([]);
+  const [done, setDone] = useState(undefined);
+
+  return (
+
+    <>
+       
+        <ReactLoading
+          
+          type={"bars"}
+          color={"#03fc4e"}
+          height={100}
+          width= {100}
+          
+        />
+       
+    </>
+  );
+}
+
+export default PreLoader1;
