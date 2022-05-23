0edbb34ebbb9ad866414200af3accbd1fd49c3b2
albus <albinbabuvarghese20@gmail.com>
2022-05-23 21:35:24 +0530

    contract deployed

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,9 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 require("babel-register");
 require("babel-polyfill");
+require("dotenv").config();
+const privateKey = process.env.PRIVATE_KEY || "";
 
 module.exports = {
   networks: {
@@ -7,6 +11,17 @@ module.exports = {
       host: "127.0.0.1",
       port: process.env.PORT || 7545,
       network_id: "*", // Match any network id
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKey,
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+        );
+      },
+      gas: 6721975, //from ganache-cli output
+      gasPrice: 20000000000, //From ganache-cli output
+      network_id: 3,
     },
   },
   contracts_directory: "./src/contracts/",
